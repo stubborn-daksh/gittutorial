be1760a12851eb634899648a07129c368af45cc6
= <=>
2023-02-01 22:17:12 +0530

    fdsf

---
+++ dom.js
@@ -0,0 +1,18 @@
+//Examine the document object
+        // console.log(document);
+        // console.log(document.domain);
+        // console.log(document.URL);
+        // console.dir(document.body);
+        // console.dir(document.title);
+        // console.log(document.doctype);
+        // console.log(document.head);
+        // console.log(document.body);
+        // console.log(document.all);
+        // console.log(document.all[10]);
+        // document.all[10].textContent = "Daksh";
+        // console.log(document.forms[0]);
+        // console.log(document.links);
+        // console.log(document.images);
+//GetElementById
+var header = document.getElementById('main-header');
+console.log(header);
