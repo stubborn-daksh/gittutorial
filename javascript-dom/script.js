9da73038e5797d72c16471f27882d29755e018de
= <=>
2023-02-02 10:51:16 +0530

    sdf

--- javascript-dom/script.js
+++ javascript-dom/script.js
@@ -1,3 +1,35 @@
-var ans1 = document.getElementById("js1");
-document.write(ans1);
-console.log(ans1);
+// var ans1 = document.getElementById("js1");
+// document.write(ans1);
+// console.log(ans1);
+
+// var ans2 = document.getElementsByTagName("*");
+// console.log(ans2);
+// document.write(ans2);
+
+// var ans3 = document.body.getElementsByTagName('*');
+// console.log(ans3);
+
+// var ans4 = document.getElementById("js1");
+// var ans5 = ans4.document.getElementsByTagName('div');
+// console.log(ans4);
+// OR
+
+// var ans5 = document.getElementById("header").document.getElementsByTagName('ul');
+// console.log(ans5);
+
+// var ans6 = document.getElementsByTagName("li").length;
+// console.log(ans6);
+
+// var len = document.getElementsByName("li");
+// var dat = len.length;
+// document.write(dat);
+// //or
+// for(let i=0; i<len; i++){
+//     var result = document.getElementsByName("li")[i];
+//     document.write(result +"<br>");
+// }
+
+//Get Elements by class name
+var res7 = document.getElementsByClassName("header");
+console.log(res7);
+
