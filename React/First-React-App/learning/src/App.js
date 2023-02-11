a1390363a54cf3ea74346ac5f10d58de9edd2c3d
= <=>
2023-02-11 22:26:43 +0530

    fsadfS

--- React/First-React-App/learning/src/App.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-let am = React.createElement("h2",null,"Hi He is Johnny Sins");
-
-export default am;
--- React/First-React-App/learning/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"; //import React library on page
-import { ReactDOM } from "react"; //import library for rendering a code on browser
-//importing a file from another page
-import am from "./App";
-//let el = <h1>Hello Daksh It is your first React Project</h1>
-let el1 = React.createElement("h1",null,"Daksh Kesarwani");
-
-ReactDOM.render(el1, document.getElementById("root"));
-ReactDOM.render(am , document.getElementById("num"));
